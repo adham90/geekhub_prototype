695a0caeb0d21f2aa1bbac84074c0a15c8b06f4e
Adham eldeeb <adham.eldeeb90@gmail.com>
2015-02-23 20:58:43 +0200

    use quotaguard

--- config/initializers/geocoder.rb
+++ config/initializers/geocoder.rb
@@ -1,1 +1,21 @@
-Geocoder.configure(:lookup => :google_premier, :api_key => [ENV["GMAP_KEY"], ENV["GMAP_CLIENT"], ENV["GMAP_CHANNEL"]])
+Geocoder.configure(
+  # geocoding options
+  :timeout      => 5,           # geocoding service timeout (secs)
+  # :lookup       => :google,     # name of geocoding service (symbol)
+  # :language     => :en,         # ISO-639 language code
+  # :use_https    => false,       # use HTTPS for lookup requests? (if supported)
+  :http_proxy   => ENV['QUOTAGUARD_URL'],  # HTTP proxy server (user:pass@host:port)
+  # :https_proxy  => nil,         # HTTPS proxy server (user:pass@host:port)
+  # :api_key      => [ENV["GMAP_KEY"], ENV["GMAP_CLIENT"], ENV["GMAP_CHANNEL"]],
+  # :cache        => nil,         # cache object (must respond to #[], #[]=, and #keys)
+  # :cache_prefix => "geocoder:", # prefix (string) to use for all cache keys
+
+  # exceptions that should not be rescued by default
+  # (if you want to implement custom error handling);
+  # supports SocketError and TimeoutError
+  # :always_raise => [],
+
+  # calculation options
+  # :units     => :mi,       # :km for kilometers or :mi for miles
+  # :distances => :linear    # :spherical or :linear
+)
